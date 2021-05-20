0eb9dc0440a76fe2353d28f86347f7734b3aed75
Ishaan Gupta <ishaangupta@Ishaans-MacBook-Pro-2.local>
2021-05-20 07:07:42 -0700

    part1

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,31 @@
  * @jest-environment jsdom
  */
 
+import { pushToHistory } from '../scripts/router'
+
+describe('history stack',()=>{
+    test('settings branch',()=>{
+        pushToHistory('settings')
+        expect(window.history.length).toBe(2)
+        expect(window.history.state.page).toBe("settings")
+    })
+    test('entry branch',()=>{
+        pushToHistory('entry', 69) 
+        expect(window.history.length).toBe(3)
+        expect(window.history.state.page).toBe("entry69")
+    })
+    test('all branches',()=>{
+        
+        expect(window.history.length).toBe(3)
+
+        pushToHistory('settings')
+        expect(window.history.length).toBe(4)
+        expect(window.history.state.page).toBe("settings")
+
+        pushToHistory('home')
+        expect(window.history.length).toBe(5)
+        expect(window.history.state.page).toBeUndefined()
+
+    })
+
+})
